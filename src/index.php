aaead24a16937dd486c0ee8e32ae1a119e88ce48
JJ <jedi.dookun@outlook.com>
2018-05-24 13:39:53 -0700

    Added project files

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>JJD Web Development</title>
+</head>
+
+<body>
+  <?php
+    include("inc/_header.html");
+    include("inc/_hero.html");
+  ?>
+</body>
+
+</html>
